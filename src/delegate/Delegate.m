9d0735a58d29c6ac5816b513ca1ab65b9a059f6a
Michael Sedlaczek <michael@gonecoding.com>
2012-02-08 11:33:35 +0100

    Removed percent escaping from device name and model
    Escaping the whole URL string has been escaping the device name and model twice leading to unwanted results

--- src/delegate/Delegate.m
+++ src/delegate/Delegate.m
@@ -53,8 +53,8 @@
 			[defaults setObject:deviceUuid forKey:@"deviceUuid"];
 		}
 	}
-	NSString *deviceName = [dev.name stringByAddingPercentEscapesUsingEncoding: NSUTF8StringEncoding];
-	NSString *deviceModel = [dev.model stringByAddingPercentEscapesUsingEncoding: NSUTF8StringEncoding];
+	NSString *deviceName = dev.name;
+	NSString *deviceModel = dev.model;
 	NSString *deviceSystemVersion = dev.systemVersion;
 	
 	// Prepare the Device Token for Registration (remove spaces and < >)
